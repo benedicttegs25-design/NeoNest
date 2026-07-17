cbbd7d2c4bbd35562c720efc66a7bda9544a2606
benedicttegs25-design <benedicttegs25@gmail.com>
2026-07-17 16:43:31 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,4 +6,94 @@ hamburger.addEventListener('click' , () => {
     hamburger.classList.toggle('active')
     navMenu.classList.toggle('active')
 
-})
+});
+
+// modal
+
+const openBtn = document.getElementById('openBtn') 
+
+const closeBtn = document.getElementById('closeBtn')
+
+const modal = document.getElementById('modal')
+
+openBtn.addEventListener('click' , () => {
+    modal.classList.remove('hidden');
+});
+
+closeBtn.addEventListener('click' , () => {
+    modal.classList.add('hidden');
+});
+
+
+
+const openBtn2 = document.getElementById('openBtn2') 
+
+const closeBtn2 = document.getElementById('closeBtn2')
+
+const modal2 = document.getElementById('modal2')
+
+openBtn2.addEventListener('click' , () => {
+    modal2.classList.remove('hidden');
+});
+
+closeBtn2.addEventListener('click' , () => {
+    modal2.classList.add('hidden');
+});
+
+const openBtn3 = document.getElementById('openBtn3') 
+
+const closeBtn3 = document.getElementById('closeBtn3')
+
+const modal3 = document.getElementById('modal3')
+
+openBtn3.addEventListener('click' , () => {
+    modal3.classList.remove('hidden');
+});
+
+closeBtn3.addEventListener('click' , () => {
+    modal3.classList.add('hidden');
+});
+
+
+const openBtn4 = document.getElementById('openBtn4') 
+
+const closeBtn4 = document.getElementById('closeBtn4')
+
+const modal4 = document.getElementById('modal4')
+
+openBtn4.addEventListener('click' , () => {
+    modal4.classList.remove('hidden');
+});
+
+closeBtn4.addEventListener('click' , () => {
+    modal4.classList.add('hidden');
+});
+
+
+const openBtn5 = document.getElementById('openBtn5') 
+
+const closeBtn5 = document.getElementById('closeBtn5')
+
+const modal5 = document.getElementById('modal5')
+
+openBtn5.addEventListener('click' , () => {
+    modal5.classList.remove('hidden');
+});
+
+closeBtn5.addEventListener('click' , () => {
+    modal5.classList.add('hidden');
+});
+
+const openBtn6 = document.getElementById('openBtn6') 
+
+const closeBtn6 = document.getElementById('closeBtn6')
+
+const modal6 = document.getElementById('modal6')
+
+openBtn6.addEventListener('click' , () => {
+    modal6.classList.remove('hidden');
+});
+
+closeBtn6.addEventListener('click' , () => {
+    modal6.classList.add('hidden');
+});
